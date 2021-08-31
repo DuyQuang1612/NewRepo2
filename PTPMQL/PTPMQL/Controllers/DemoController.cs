5275f5112d2c1081215aa43b1e0bf91c7b787a30
DuyQuang1122 <77633978+DuyQuang1122@users.noreply.github.com>
2021-08-31 10:45:03 +0700

    2bug

---
+++ PTPMQL/PTPMQL/Controllers/DemoController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using PTPMQL.Models;
+
+namespace PTPMQL.Controllers
+{
+    public class DemoController : Controller
+    {
+        giaiPT ketthuc = new giaiPT();
+        // GET: Demo
+        public ActionResult giai()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Giai(string soA, string soB)
+        {
+            double so1 = Convert.ToDouble(soA);
+            double so2 = Convert.ToDouble(soB);
+            double soX = ketthuc.giaiPTbac1(so1, so2);
+            ViewBag.x = soX;
+        }
+    }
+}
---
+++ PTPMQL/PTPMQL/Models/giaiPT.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PTPMQL.Models
+{
+    public class giaiPT
+    {
+        double giaiPTbac1(double a,double b)
+        {
+            double x=0;
+             x= -b/a;
+            return x;
+        }
+    }
+}
