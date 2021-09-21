212591e31b8677be6bece2c81ab6d28779a770b6
DuyQuang1122 <77633978+DuyQuang1122@users.noreply.github.com>
2021-09-21 10:18:42 +0700

    2192021

---
+++ PTPMQL/PTPMQL/Migrations/202109210307412_IntialCreate.cs
@@ -0,0 +1,37 @@
+﻿namespace PTPMQL.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class IntialCreate : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Accounts",
+                c => new
+                    {
+                        Username = c.String(nullable: false, maxLength: 128, unicode: false),
+                        Password = c.String(),
+                    })
+                .PrimaryKey(t => t.Username);
+            
+            CreateTable(
+                "dbo.Students",
+                c => new
+                    {
+                        StudentID = c.Int(nullable: false, identity: true),
+                        StudentName = c.Int(nullable: false),
+                        Adress = c.Int(nullable: false),
+                    })
+                .PrimaryKey(t => t.StudentID);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Students");
+            DropTable("dbo.Accounts");
+        }
+    }
+}
---
+++ PTPMQL/PTPMQL/Models/LTQLdbcontext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace PTPMQL.Models
+{
+    public partial class LTQLdbcontext : DbContext
+    {
+        public LTQLdbcontext() : base("name=LTQLdbcontext")
+        {
+        }
+        public virtual DbSet<Account> Accounts { get; set; }
+        public virtual DbSet<Student> Students { get; set; }
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Account>() .Property(e => e.Username).IsUnicode(false);
+            modelBuilder.Entity<Student>() .Property(e => e.StudentID);
+        }
+    }
+}   
